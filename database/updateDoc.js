79e7dd03e2174a0d978c8963c3e100a5408afabd
Nicholas Farm <nicholasfarm27@gmail.com>
2022-02-10 18:58:26 +0800

    Add nodemon

---
+++ database/updateDoc.js
@@ -0,0 +1,26 @@
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase.js";
+
+const testRef = doc(db, "Users", "9RVPolmrAAYaemxuhoxQSE7sZxY2");
+const addRef = doc(db, "posts", "1460661598536744965")
+
+// Set the "capital" field of the city 'DC'
+// await updateDoc(testRef, {
+//   ref: addRef
+// });
+
+try {
+    const docSnap = await getDoc(testRef);
+
+    if (docSnap.exists()) {
+        const newRef = docSnap.data().ref
+        // console.log({data: docSnap.data().ref})
+        const newDocSnap = await getDoc(newRef)
+        console.log(newDocSnap.data())
+
+    } else {
+        console.log({message: "No document found"})
+    }
+} catch (error) {
+    console.log({error: error})
+}
